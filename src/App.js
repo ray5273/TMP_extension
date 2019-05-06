195db13bbf437adfeb7b6ff52e2ba57dd0e9e24c
sieun <tlatldms@naver.com>
2019-05-06 13:31:14 +0900

    to eject

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import Memo from './components/Main_Menu'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Header from './components/Header';
-import Signin from './components/Signin'
-import Memo_test from './components/Content_Memo'
+import Signin from './components/Signin';
+import Memo_test from './components/Content_Memo';
+import MainMenu from './components/Main_Menu';
 
 class App extends Component {
   render() {
     return (
       <Router>
+        {/* 
         <div>
           <Header />
           <div>
@@ -20,6 +22,8 @@ class App extends Component {
             </Switch>
           </div>
         </div>
+        */}
+        <MainMenu />
       </Router>
     );
   }
