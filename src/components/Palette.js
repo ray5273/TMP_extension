49457088e3ec44f244f6c99ab8cfecc1e4c6d6c9
sieun <tlatldms@naver.com>
2019-05-19 10:59:42 +0900

    temp menu bar css

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Icon from '../assets/marker-icon.png';
 
 const Button = styled.button`
     background-color: ${props => props.color};
@@ -33,10 +34,11 @@ class Palette extends Component {
         }
     }
     render() {
-        const colors=["#FBD6C6","#F35ADF", "#FFDF24", "#FFFF7E", "#BCFFB5", "#C5C0FF", "#FFD9FA",
-        "#B8B8B8", "#489CFF"]
+        const colors=["#FBD6C6","#FF9090", "#FFDF24", "#FFFF7E","#7ED2FF", "#BCFFB5", "#C5C0FF", "#FFD9FA",
+        "#B8B8B8" ]
         return (
             <div className="palette-container">
+                <img alt="" src={Icon} className="Highlight-icon" />
                 {colors.map(
                     color =>
                     <Button className="palette-item" onClick={(e)=>this.addHighlight(e, color)} color={color}></Button>
