5c378105b361ea285caeb6f0cde5b55a04a96d7a
sanghyeok <ray5273@naver.com>
2019-06-07 17:29:20 +0900

    Save image and meta data on path containing uid and url

--- src/components/Memo/DrawingTool.js
+++ src/components/Memo/DrawingTool.js
@@ -22,6 +22,7 @@ function dataURItoBlob(dataURI)
 
     return new Blob([ia], {type:mimeString});
 }
+
 class DrawingTool extends Component {
     addDrawingMemo = () => {
 
@@ -30,36 +31,76 @@ class DrawingTool extends Component {
         var storage = Firebase.storage();
         var storage_ref = storage.ref();
         //TODO:currentUser 인식 안되는 문제 - 현재는 우리 어플 로그인 인식이 안되어도 파일이 올려지도록,
-        console.log("in drawing tool : uid : "+this.props.uid +" url : "+this.props.url);
+        var uid = this.props.uid;
+        var url = this.props.url;
 
+        //firestore 에 memo갯수를 저장해두어야할듯
         var database = Firebase.firestore();
-        database.collection("cdkfdkfds").doc("LAskdakdjsd").set({
-            name: "Los Angelesddd",
-            state: "CAasdsd",
-            country: "USAs"
-        }).then(function() {
-            console.log("Document successfully written!");
-        }).catch(function(error) {
-            console.error("Error writing document: ", error);
+
+        // 주소 URL이 path를 의미하는 / 를 포함하기때문에 인코딩 디코딩 과정 추가
+        url = encodeURIComponent(url);
+        console.log("in drawing tool : uid : "+this.props.uid +" encoded url : "+url);
+        console.log("decoded url : "+ decodeURIComponent(url));
+
+
+        //여기서 url에 대한 image memo count가 존재하는지먼저 파악 없을시 새로 만들어줌.
+        database.collection("User").doc(uid).collection(url).doc("MemoImage").get().then(function(doc){
+            if(doc.exists){
+                console.log(doc.id,"=>",doc.data());
+            }else{
+                database.collection("User").doc(uid).collection(url).doc("MemoImage").set({
+                    imageCount: 0
+                }).then(function() {
+                    console.log("Memo Count firestore successfully added");
+                }).catch(function(error) {
+                    console.error("Error writing document: ", error);
+                });
+            }
         });
-       var ptro = Painterro({
-           saveHandler : function(image,done){
 
-               //TODO: User 인식 후 storage directory 변경해주기
-               //TODO: storage directory 변경 후 metadata도 firestore에 저장 해주어야할듯
-               var dataUrl = image.asDataURL();
-               var blob = dataURItoBlob(dataUrl);
-               var dataRef = storage_ref.child('data.png');
-               dataRef.put(blob).then(function (snapshot) {
-                   console.log("uploaded a file!");
-                   done(true);
-               });
-           }
-       });
+        // url에 image 를 추가하고 해당 url의 memocount를 늘려줌
+        var ptro = Painterro({
+            saveHandler : function(image,done){
+                //TODO: User 인식 후 storage directory 변경해주기
+                //TODO: storage directory 변경 후 metadata도 firestore에 저장 해주어야할듯
+                var dataUrl = image.asDataURL();
+                var blob = dataURItoBlob(dataUrl);
+
+                //metadata에 x,y좌표 초기화 : (0,0)
+                //각각의 url에서 image는 1번부터 시작 ex) image1.png ...
+                database.collection("User").doc(uid).collection(url).doc("MemoImage").get().then(function(doc){
+                        console.log("value : "+doc.data().imageCount);
+                        var image_data_num = doc.data().imageCount+1;
+                        database.collection("User").doc(uid).collection(url).doc("MemoImage").set({
+                            imageCount: image_data_num
+                        }).then(function() {
+                            console.log("Document successfully written!");
+                            var imagePath = uid +'/'+url+'/'+'image'+image_data_num+'.png';
+                            var dataRef = storage_ref.child(imagePath);
+                            var posMetadata = {
+                                customMetadata: {
+                                    'x':0,
+                                    'y':0
+                                }
+                            };
+                            dataRef.put(blob).then(function (snapshot) {
+                                console.log("uploaded a file!");
+                                done(true);
+                                dataRef.updateMetadata(posMetadata).then(function(metadata){
+                                    console.log("meta data pos : 0,0");
+                                }).catch(function(error){
+                                });
+                            });
+
+
+                        }).catch(function(error) {
+                            console.error("Error writing document: ", error);
+                        });
+                });
+            }
+        });
         ptro.show();
-
     };
-
     render() {
         return (
             <div className="menus" onClick={this.addDrawingMemo}>
