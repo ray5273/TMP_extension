6175d69227f90931c388f7b2568e0253a7e4caf7
Hyolim Shin <9657493@naver.com>
2019-06-11 02:00:56 +0900

    Simple Search

--- src/components/SimSearch.js
+++ src/components/SimSearch.js
@@ -3,33 +3,124 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import firebase from '../Firebase';
 import 'firebase/firestore';
+import styled, { css } from 'styled-components';
 import MenuBar from "./MenuBar";
+
+const Logo = styled.p`
+    font-size: 1.4rem;
+    color:#ffffff;
+    font-family    : AppleSDGothicNeo-Regular, "Malgun Gothic", "맑은 고딕", dotum, 돋움, sans-serif;
+
+`;
 
 class SimSearch extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            abc: document.getElementsByName('q')[0].value
+            abc: document.getElementsByName('q')[0].value,
+            search: []
         }
     }
     componentDidMount(): void {
-        console.log("uid in Simsearch : "+this.props.uid);
+        console.log("uid in Simsearch : " + this.props.uid);
+
+        var uid = this.props.uid;
+        var url = this.props.url;
+
+        url = encodeURIComponent(url);
+
+        var db = firebase.firestore();
+
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
-                this.setState({ user: user });
+                this.setState({user: user});
                 var curUser = user.uid;
-                console.log("SimSearch: User value:"+curUser);
+                console.log("SimSearch: User value:" + curUser);
 
             } else {
-                this.setState({ user: user });
+                this.setState({user: user});
                 console.log("SimSearch: Signed Out");
             }
         });
+
+        /*db.collection("User").doc(uid).collection("Url").doc("https%3A%2F%2Fjechue.tistory.com%2F55").get().then(docs => {
+            docs.data().memos.forEach(memo => {
+                console.log(memo.y);
+            })
+        });
+        console.log("됐나?");
+
+        console.log(this.state.abc);*/
+        //User(col)-UID(doc)-Url(col)-Url1(doc)-field(memos:[{},{},{}])의 db structure 일 때
+        /*db.collection("User").doc(uid).collection("Url").get()
+            .then(docs=>{
+                console.log(docs);
+                docs.forEach(doc=>{
+                    console.log(doc);
+                    console.log(doc.id);
+                    console.log(doc.data());
+                    console.log(doc.data().memos);
+                    doc.data().memos.forEach( memo => {
+                        console.log(memo);
+                        let temp = memo.content;
+                        console.log(temp);
+                        temp = temp.replace(/(\s*)/g,"");
+
+                        if(temp.indexOf(this.state.abc) !== -1){
+                            this.setState({
+                                search: this.state.search.concat({content: memo.content, url: doc.data().url, title: doc.data().title})
+                            })
+                        }
+                    });
+                })
+            });*/
+        db.collection("User").doc(uid).collection("Urll").get()
+            .then(docs => {
+                console.log(docs);
+                docs.forEach(doc => {
+                    console.log(doc);
+                    db.collection("User").doc(uid).collection("Urll").doc(doc.id).collection("Memo").get()
+                        .then(memos => {
+                            console.log(memos);
+                            memos.forEach(memo => {
+                                let temp = memo.data().content;
+                                console.log(temp);
+                                temp = temp.replace(/(\s*)/g,"");
+
+                                if(temp.indexOf(this.state.abc) !== -1){
+                                    this.setState({
+                                        search: this.state.search.concat({content: memo.data().content, url: memo.data().url, title: memo.data().title})
+                                    })
+                                }
+                            })
+                        })
+                })
+            });
     }
+
     render() {
+        let j = 0;
+        const open = this.state.search.map((memo,i) =>{
+            j = j + 1;
+            return(
+                //<React.Fragment key = {i}>
+                <div key = {i} className="search3" onClick={()=>window.open(memo.url)}>
+                    <div className="search4"> {memo.title} </div>
+                    <div className="search5"> {memo.content} </div>
+                </div>
+                //</React.Fragment>
+            )
+        });
         return (
             <React.Fragment>
                 <div className="search1">
+                    Trendy Memo Project
+                </div>
+                <div className="search2">
+                    {j === 0 ? <div className="search6"> 관련된 데이터가 없습니다. </div> : open}
+                </div>
+            </React.Fragment>
+        /*<div className="search1">
                     <p>TMP</p>
                 </div>
                 <div className="search2">
@@ -46,7 +137,7 @@ class SimSearch extends Component {
                     </div>
                 </div>
 
-            </React.Fragment>
+            </React.Fragment>*/
         );
     }
 }
@@ -62,7 +153,7 @@ check2.insertBefore(check1, check);
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(request.message==="getID"){
-            ReactDOM.render(<SimSearch uid={request.id}/>, check1);
+            ReactDOM.render(<SimSearch uid={request.id} url={request.url}/>, check1);
         }
     }
 );
