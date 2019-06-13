9b23ffbc2b54da59224bfa490da209322ccb8ead
Sunghyun Hwang <zailbreak@gmail.com>
2019-06-13 15:04:10 +0900

    Add max icon in iframe Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -13,12 +13,12 @@ class Item extends Component {
     render() {
         const { url, name, content, fid, handleRemove } = this.props;
         return (
-            <div className={`memo-item-wrapper ${this.state.overClicked ? "over" : ""}`} onClick={()=>window.open(url)} >
+            <div className={`memo-item-wrapper ${this.state.overClicked ? "over" : ""}`}>
                 <div className="memo-top-wrapper">
-                    <div className={`iframe-memo-title ${this.state.overClicked ? "over" : ""}`}>{name}</div>
+                    <div className={`iframe-memo-title ${this.state.overClicked ? "over" : ""}`} onClick={()=>window.open(url)}>{name}</div>
                     <div className="iframe-memo-buttons-container">
                         {content.length > 100 ?
-                            <img className="max-icon" alt="" src={Max} onClick={() => this.setState({ overClicked: !this.state.overClicked })} />
+                            <img className="iframe-memo-max-button" alt="" src={Max} onClick={() => this.setState({ overClicked: !this.state.overClicked })} style={{margin: 5 + "px"}}/>
                             :
                             null
                         }
@@ -26,12 +26,12 @@ class Item extends Component {
                         <img src={XIcon} alt="" onClick={(e) => {
                             e.stopPropagation();
                             handleRemove(fid, url)
-                        }} className="iframe-memo-xbutton"
+                        }} className="iframe-memo-x-button" style={{margin: 5 + "px"}}
                             />
                     </div>
                 </div>
                 
-                <div className={`iframe-memo-content ${this.state.overClicked ? "over" : ""}`}>{content}</div>
+                <div className={`iframe-memo-content ${this.state.overClicked ? "over" : ""}`} onClick={()=>window.open(url)}>{content}</div>
 
                 {/*<div className="url-container">*/}
                 {/*    <img className="clip-image" src={Clip} alt="" />*/}
